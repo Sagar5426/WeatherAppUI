4204bdce4b02758f3f41e858a446c61eab554e6a
Sagar Jangra <108585817+Sagar5426@users.noreply.github.com>
2024-05-08 15:19:07 +0530

    Add files via upload

---
+++ SwiftUI-WeatherApp/SwiftUI-WeatherApp/weatherButton.swift
@@ -0,0 +1,24 @@
+//
+//  weatherButton.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Sagar Jangra on 08/05/2024.
+//
+
+import SwiftUI
+
+struct weatherButton : View{
+    
+    let title : String
+    var textColor : Color
+    var backgroundColor : Color
+    
+    var body : some View{
+            Text(title)
+                .frame(width: 280, height: 50)
+                .background(backgroundColor)
+                .foregroundStyle(textColor)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
